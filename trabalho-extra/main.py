8e6a9a5fd34be7277df40504af9fd96abce61f5a
GDevigili <gianluccadevigili@gmail.com>
2020-09-28 17:53:09 -0300

    adição dos arquivos das classes

--- trabalho-extra/main.py
+++ trabalho-extra/main.py
@@ -1,22 +1,22 @@
 import os
 
 class FileDict(dict):
-  """
-  File based dict sub-class
-  filepath [str]: path to the core file of the dict
-  """
+    """
+    File based dict sub-class
+    filepath [str]: path to the core file of the dict
+    """
     def __init__(self,filepath,**kwargs):
-      """Constructor method"""
-      pass
+        """Constructor method"""
+        pass
         
     def __setitem__(self, key, value):
-      """Creates a new item in the dict"""
-      pass
-
+        """Creates a new item in the dict"""
+        pass
+    
     def pop(self,key):
-      """Removes an item by the key"""
-      pass
-
+        """Removes an item by the key"""
+        pass
+    
     def __del__(self):
-      """Delete the dict and the file"""
-      pass
+        """Delete the dict and the file"""
+        pass
