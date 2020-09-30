94108972f98ffda39f719a807c61edf20f991bcc
GDevigili <gianluccadevigili@gmail.com>
2020-09-29 22:46:33 -0300

    adição do init de FileDict

--- trabalho-extra/main.py
+++ trabalho-extra/main.py
@@ -5,13 +5,35 @@ class FileDict(dict):
     File based dict sub-class
     filepath [str]: path to the core file of the dict
     """
+    #defines the separator for all FileDict objects
+    SEP = ":" 
     def __init__(self,filepath,**kwargs):
         """Constructor method"""
-        pass
+        #defines the variables
+        self.filepath = filepath
+        self.dict = {}
+
+        file = open(self.filepath, 'r')
+        
+        file_text = file.read()
+        lines = file_text.split("\n")
+        lines.remove("")
+        
+        for line in lines:
+            key, value = line.split(self.SEP)
+            self.dict[key] = value
         
     def __setitem__(self, key, value):
         """Creates a new item in the dict"""
-        pass
+        '''self.dict[key] = value
+    
+        file = (self.filepath, 'a')
+        
+        file_text = file.read()
+        lines = file_text.split("\n")
+        lines.remove("")
+        '''
+        
     
     def pop(self,key):
         """Removes an item by the key"""
@@ -20,3 +42,8 @@ class FileDict(dict):
     def __del__(self):
         """Delete the dict and the file"""
         pass
+
+file_dict = FileDict("dicio.txt")
+#print(file_dict.dict['chave1'])
+print(file_dict)
+#print(dickt.file_text)
