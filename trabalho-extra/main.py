92258f4b3e96a573adf43c844bafb389d21f0e7d
GDevigili <gianluccadevigili@gmail.com>
2020-10-01 10:29:15 -0300

    Trabalho extra completo

--- trabalho-extra/main.py
+++ trabalho-extra/main.py
@@ -31,13 +31,14 @@ class FileDict(dict):
                 lines.remove("")
             except ValueError:
                 pass
-            
+
             for line in lines:
                 try:
                     key, value = line.split(self.SEP)
                     self[key] = value
                 except ValueError:
                     pass
+                    
             file.close()
              
         except FileNotFoundError:
@@ -45,34 +46,70 @@ class FileDict(dict):
             file.close()
             #print("File not found")
             pass
+
+        #add the kwargs to the file      
+        if kwargs != {}:
+
+            file = open(self.filepath, "r")
+            file_text = file.read()
+            file.close()
+
+            file = open(self.filepath, 'w')
+            for key in kwargs:
+                file_text += str("\n" + key + self.SEP + kwargs.get(key, None))
+                
+            file.write(file_text)        
+            file.close()
+            
         
         
     def __setitem__(self, key, value):
         """Creates a new item in the dict"""
-        self[key] = value
-    
-        file = open(self.filepath, 'a')
-        dict_element = str("\n" + key + ":" + value)
-        file.write(dict_element)
-        
+        super().__setitem__(key, value)
+        #self[key] = value
+
+        file = open(self.filepath, 'r')
+            
+        file_text = file.read()
+        lines = file_text.split("\n")
         file.close()
+
+        dict_element = str(key + self.SEP + value)
+
+        if dict_element not in lines:
+            file = open(self.filepath, 'a')
+            file.write("\n" + dict_element)
+            file.close()
+        
         
     def pop(self,key):
         """Removes an item by the key"""
-        
         try:
-            self.pop(key)
+            value = super().pop(key)
         
-            file = open(self.filepath, 'w+')
-            
+            file = open(self.filepath, 'r')
+            file_text = file.read()
+            lines = file_text.split("\n")
             file.close()
-            
+            file_text = ""
+            for i in lines:
+                if key in i:
+                    pass
+                else:
+                    file_text += i + "\n"
+                    file
+            file = open(self.filepath, 'w')
+            file.write(file_text)
+            file.close()
+
+            return value
         except KeyError:
             print("KeyError")
-    
+
     def __del__(self):
         """Delete the dict and the file"""
-        
+        #Se tiver comentado é porque esqueci de tirar o comentário#tive que comentar pra testar se o resto estava funcionando
+
         os.remove(self.filepath)
         del self
         
@@ -80,13 +117,13 @@ class FileDict(dict):
 
 #TESTES
 
-file_dict = FileDict("dicio.txt") # -->(1)
+file_dict = FileDict("dicio.txt", chave6 = "elemento6") # -->(1)
 print(file_dict)
 file_dict["chave5"] = "elemento5" # -->(2)
 print(file_dict)
 file_dict.pop('chave1') # -->(3)
 print(file_dict)
-file_dict = FileDict("dicioo.txt")
+file_dict = FileDict("dicioo.txt", chave6 = "elemento6")
 
 '''
 (1) Cria o dicionário
