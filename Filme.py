7acd1500cc21a2d90f9bd4d25ff2ca109872468b
GDevigili <gianluccadevigili@gmail.com>
2020-10-01 11:35:06 -0300

    conclusão da classe Sala
    também adicionei a base de Filme e Sessao.
    Transferi o código entre os outros arquivos que foram editados

--- Filme.py
+++ Filme.py
@@ -1,2 +1,5 @@
-class Filme:
-    pass
+class Filme():
+  def __init__(self, titulo, classificacao, duracao): 
+    self.titulo = titulo
+    self.classificacao = int(classificacao)
+    self.duracao = int(duracao)
