8202bec0cb1464b7fc99242c11a92f1b4087ffaa
Raphael Levy <raphael.levy147@gmail.com>
2020-10-01 17:23:31 -0300

    Classe Guerreiro

--- Guerreiro.py
+++ Guerreiro.py
@@ -1,1 +1,22 @@
-from Sala import Sala
+#from Sala import Sala
+
+class Guerreiro:
+    def __init__(self, nome, idade): 
+        self._nome = nome
+        self._idade = int(idade)
+
+    @property
+    def nome(self):
+      return self._nome
+            
+    @nome.setter
+    def set_nome(self, apelido):
+         self._nome = apelido
+         
+    @property
+    def idade(self):
+        return self._idade
+    
+    @idade.setter
+    def set_idade(self, anos):
+         self._idade = int(anos)
