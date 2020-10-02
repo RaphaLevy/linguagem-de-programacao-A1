819ab50760ebc2f398b8a4b83a85d9de4cc922cf
GDevigili <gianluccadevigili@gmail.com>
2020-10-02 15:43:05 -0300

    .

--- mod.py
+++ mod.py
@@ -1,6 +1,39 @@
 def comprar_ingresso(salas):
+    mostrar_sessoes(salas)
+    input("favor, insira o número da sala:")
+    
+    
+        
+def mostrar_sessoes(salas):
     cont = 0
     for cada_sala in salas:
-        for cada_sessao in cada_sala.sessoes:
-            cont += 1
-            pass
+        cont += 1
+        print(f"---Sala{cont}---")
+        cada_sala.verificar_programacao()
+        print("-----------")
+
+def iniciar():
+    print("""
+                   /^\\
+          _.-`:   /   \\   :'-._
+        ,`    :  |     |  :    '.
+      ,`       \,|     |,/       '.
+     /   BEM     `-...-`   VINDO  \\
+    :              .'.              :
+    |             . ' .             |
+    |             ' AO'             |
+    :              '.'              :
+     \           ,-___-,           /
+      `.       /'|     |'\       ,'
+        `._   ;  |     |  ;   _,'
+           `-.:  |     |  :,-'
+                 |     |
+        ____ _ _  _ ____ _  _ ____   
+        |    | |\ | |___ |\/| |__|    
+        |___ | | \| |___ |  | |  |     
+    _  _ ____ _    _  _ ____ _    _    ____ 
+    |  | |__| |    |__| |__| |    |    |__|    
+     \/  |  | |___ |  | |  | |___ |___ |  |                                                                   
+                
+""")
+        
